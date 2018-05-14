3cff39eadf09e054a0db6ff27093f2957efbbb2c
sarah bortoloti <sarah.bortoloti.15@gmail.com>
2018-05-14 15:44:37 -0300

    Initial commit

---
+++ src/main/java/first/project/spring/ProjetoSpring2Application.java
@@ -0,0 +1,12 @@
+package first.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring2Application.class, args);
+	}
+}
