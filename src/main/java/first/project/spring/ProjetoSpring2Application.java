9bc66ad9d123ae9aed22b9d3807cd69fb4323ed6
sarah bortoloti <sarah.bortoloti.15@gmail.com>
2018-05-14 18:00:11 -0300

    Criando  operação de instaniação

--- src/main/java/first/project/spring/ProjetoSpring2Application.java
+++ src/main/java/first/project/spring/ProjetoSpring2Application.java
@@ -1,12 +1,27 @@
 package first.project.spring;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSpring2Application {
+import first.project.spring.domain.Categoria;
+import first.project.spring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoSpring2Application implements CommandLineRunner {
+
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpring2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 =new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
