3ff00dba436fc415e22f1ad9c068d47b87c39a53
sarah bortoloti <sarah.bortoloti.15@gmail.com>
2018-05-16 16:38:39 -0300

    melhoria update

--- src/main/java/first/project/spring/resources/exceptions/ValidationError.java
+++ src/main/java/first/project/spring/resources/exceptions/ValidationError.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class ValidationError extends StandardError{
 
 	private static final long serialVersionUID = 1L;
-	private List<FieldMessage> list = new ArrayList<>();
+	private List<FieldMessage> errors = new ArrayList<>();
 	public ValidationError(int status, String msg, Long timeStamp) {
 		super(status, msg, timeStamp);
 		// TODO Auto-generated constructor stub
 	}
-	public List<FieldMessage> getList() {
-		return list;
+	public List<FieldMessage> getErrors() {
+		return errors;
 	}
 	public void addError(String fieldName, String message) {
-		list.add(new FieldMessage(fieldName, message));
+		errors.add(new FieldMessage(fieldName, message));
 	}
 	
 	
--- src/main/java/first/project/spring/services/ClienteService.java
+++ src/main/java/first/project/spring/services/ClienteService.java
@@ -1,10 +1,16 @@
 package first.project.spring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import first.project.spring.DTO.ClienteDTO;
 import first.project.spring.domain.Cliente;
 import first.project.spring.repositories.ClienteRepository;
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -20,4 +26,38 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	public Cliente update(Cliente obj) throws ObjectNotFoundException {
+		Cliente newObj =find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) throws ObjectNotFoundException {
+		find(id);
+		try {
+		repo.deleteById(id);
+	}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possível excluir uma categoria que contém produto");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj){
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
 }
